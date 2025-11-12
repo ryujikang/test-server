45e9d1e6a34535b48144de0f8ca41f2b730636c9
ryujikang <dbwlrkd2000@naver.com>
2025-11-12 18:53:55 +0900

    hi

--- src/main/java/com/example/test_server/controller/AppController.java
+++ src/main/java/com/example/test_server/controller/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
+
     @GetMapping("/")
     public String home() {
         return "hi2, World!";
